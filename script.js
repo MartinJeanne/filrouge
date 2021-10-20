2c3429ad6cb5c0186c7761f0fa8cd6449cfd28ef
Martin JEANNE <mjeanne@leonard-solutions.local>
2021-10-20 09:59:29 +0200

    Styling it up a bit

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ request.send();
 request.onload = function () {
 	const data = JSON.parse(request.response);
 	console.log(data);
-	populateEmployees(data.employees);
+	//populateEmployees(data.employees);
 	populateSevervice(data.services, data.employees)
 }
 
 function populateEmployees(employees) {
 	for (let i = 0; i < employees.length; i++) {
 		const myArticle = document.createElement('article');
-		const myH2 = document.createElement('h2');
+		const myH2 = document.createElement('h4');
 		const myP = document.createElement('p');
 
 		myH2.textContent = employees[i].name;
@@ -29,15 +29,17 @@ function populateEmployees(employees) {
 function populateSevervice(services,employees) {
 	for (let i = 0; i < services.length; i++) {
 		const myArticle = document.createElement('article');
+		const myDiv = document.createElement('div');
 		const myH2 = document.createElement('h2');
 		const myP = document.createElement('p');
 		const myUl = document.createElement('ul');
 
-		myH2.textContent = services[i].name;
+		myH2.textContent = services[i].name.charAt(0).toUpperCase() + services[i].name.slice(1);
 		myP.textContent = 0;
+		myDiv.appendChild(myH2);
+		myDiv.appendChild(myP);
 		myArticle.setAttribute("id", services[i].name);
-		myArticle.appendChild(myH2);
-		myArticle.appendChild(myP);
+		myArticle.appendChild(myDiv);
 		myArticle.appendChild(myUl);
 
 		document.getElementById('services').appendChild(myArticle);
@@ -47,7 +49,7 @@ function populateSevervice(services,employees) {
 		const myP = document.createElement('li');
 
 		myP.textContent = employees[i].name;
-		let count = document.querySelector("#" + employees[i].service + ">p").textContent++;
+		document.querySelector("#" + employees[i].service + ">div>p").textContent++;
 		document.querySelector("#" + employees[i].service + ">ul").appendChild(myP);
 	}
 }

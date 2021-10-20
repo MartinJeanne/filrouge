48ac346564a4e8624c45ab4952115adec5c5fc68
Martin JEANNE <mjeanne@leonard-solutions.local>
2021-10-20 10:54:48 +0200

    Big improvement of CSS + preparing the implementation of softs in services section

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ request.onload = function () {
 	const data = JSON.parse(request.response);
 	console.log(data);
 	//populateEmployees(data.employees);
-	populateSevervice(data.services, data.employees)
+	populateService(data.services, data.employees)
 }
 
 function populateEmployees(employees) {
@@ -26,30 +26,59 @@ function populateEmployees(employees) {
 	}
 }
 
-function populateSevervice(services,employees) {
+function populateService(services,employees) {
 	for (let i = 0; i < services.length; i++) {
-		const myArticle = document.createElement('article');
-		const myDiv = document.createElement('div');
-		const myH2 = document.createElement('h2');
-		const myP = document.createElement('p');
-		const myUl = document.createElement('ul');
+		const article = document.createElement('article');
+		const h2 = document.createElement('h2');
+		const div = document.createElement('div');
 
-		myH2.textContent = services[i].name.charAt(0).toUpperCase() + services[i].name.slice(1);
-		myP.textContent = 0;
-		myDiv.appendChild(myH2);
-		myDiv.appendChild(myP);
-		myArticle.setAttribute("id", services[i].name);
-		myArticle.appendChild(myDiv);
-		myArticle.appendChild(myUl);
+		const divEmployees = document.createElement('div');
+		const divTitleEmployees = document.createElement('div');
+		const h3Employees = document.createElement('h3');
+		const countEmployees = document.createElement('p');
+		const ulEmployees = document.createElement('ul');
 
-		document.getElementById('services').appendChild(myArticle);
+		const divsofts = document.createElement('div');
+		const divTitleSofts = document.createElement('div');
+		const h3Softs = document.createElement('h3');
+		const countSofts = document.createElement('p');
+		const ulSofts = document.createElement('ul');
+
+		h2.textContent = services[i].name.charAt(0).toUpperCase() + services[i].name.slice(1);
+		countEmployees.textContent = countSofts.textContent = 0;
+
+		article.setAttribute("id", services[i].name);
+		article.appendChild(h2);
+		article.appendChild(div);
+		div.appendChild(divEmployees);
+		div.appendChild(divsofts);
+
+		divEmployees.appendChild(divTitleEmployees)
+		divEmployees.appendChild(ulEmployees);
+		divTitleEmployees.appendChild(h3Employees);
+		divTitleEmployees.appendChild(countEmployees);
+		divEmployees.setAttribute("class", 'serviceChild');
+		countEmployees.setAttribute("id", services[i].name + 'Count');
+		ulEmployees.setAttribute("id", services[i].name + 'List');
+		h3Employees.textContent = "Employees :";
+
+		divsofts.appendChild(divTitleSofts)
+		divsofts.appendChild(ulSofts);
+		divTitleSofts.appendChild(h3Softs);
+		divTitleSofts.appendChild(countSofts);
+		divsofts.setAttribute("class", 'serviceChild');
+		countSofts.setAttribute("id", services[i].name + 'Count');
+		ulSofts.setAttribute("id", services[i].name + 'List');
+		h3Softs.textContent = "Softwares :"
+		
+		document.getElementById('services').appendChild(article);
 	}
 
 	for (let i = 0; i < employees.length; i++) {
-		const myP = document.createElement('li');
+		const p = document.createElement('li');
 
-		myP.textContent = employees[i].name;
-		document.querySelector("#" + employees[i].service + ">div>p").textContent++;
-		document.querySelector("#" + employees[i].service + ">ul").appendChild(myP);
+		p.textContent = employees[i].name;
+		document.getElementById(employees[i].service + 'Count').textContent++;
+		document.getElementById(employees[i].service + 'List').appendChild(p);
 	}
 }
